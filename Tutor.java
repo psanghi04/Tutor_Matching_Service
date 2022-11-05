1c54c2ce4bdb9fc513346fb0024d95b56376247a
aschidamb <112208471+aschidamb@users.noreply.github.com>
2022-11-05 16:01:59 -0400

    Added Tutor and Student subclasses

---
+++ Tutor.java
@@ -0,0 +1,32 @@
+public class Tutor extends User {
+  private User[] users; // list of users
+  private User user; // a specific user
+
+  public Tutor(String message, String accountUsername, String email, boolean[] blockedList, User[] users, User user){
+    super(message, accountUsername, email, blockedList);
+    this.users = users;
+    this.user = user;
+  } 
+
+  // get a list of users
+  private User[] getUsers(){
+    return users;
+  }
+
+  // get a user 
+  public User getUser(){
+    return user;
+  }
+
+  // set a user list
+  private void setUsers(User[] users){
+    this.users = users;
+  }
+
+  // set the user
+  public User setUser(User user){
+    this.user = user;
+  }
+  
+  
+}
