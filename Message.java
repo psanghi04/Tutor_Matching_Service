a07254d9f1bc6852b7ef6051f290d6d874a5bb11
psanghi04 <psanghi@purdue.edu>
2022-11-05 15:19:26 -0400

    Created Message class skeleton

---
+++ Message.java
@@ -0,0 +1,14 @@
+public class Message {
+    private Tutor tutor;
+    private Student student;
+    private String fileName;
+
+    public void isValid(){} // if he is blocked/invisible and correct user
+    public void edit(){}
+    public void delete(){}
+    public void getTime(){}
+    public void readMsg(){}
+    public void writeMsg(){}
+    public void export(/*<which conversations>*/){}
+    public void existing() {}
+}
