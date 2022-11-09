5bd2715e60c1f3c40dbad801539a6a2331afffc1
aschidamb <112208471+aschidamb@users.noreply.github.com>
2022-11-09 15:13:36 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    
+    System.out.println("Welcome to Tutoring Center!");
+
+    System.out.println("Username: ");
+    String userName = scan.nextLine();
+    
+    System.out.println("Email: ");
+    String email = scan.nextLine();
+    
+    System.out.println("Password: ");
+    String password = scan.nextLine();
+    
+    
+
+    // User user = new User(username, password, email)
+  }
+}
