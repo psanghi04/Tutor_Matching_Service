4409dfb81e4c3a338e9cc5a184666adfcd91fc5d
aschidamb <112208471+aschidamb@users.noreply.github.com>
2022-11-09 15:15:56 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,9 +1,7 @@
 public class Student extends User {
 
-  public Student(String message, String accountUsername, String email, boolean[] blockedList, User[] users, User user){
+  public Student(String message, String accountUsername, String email, User[] blockedList){
     super(message, accountUsername, email, blockedList);
-    this.users = users;
-    this.user = user;
   } 
   
 }
