67458ba836692966273477f9a57663dc22af0196
aschidamb <112208471+aschidamb@users.noreply.github.com>
2022-11-09 15:33:09 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,52 @@
 import java.util.*;
 
-class Main {
+public class Main {
   public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
+    boolean login = false;
+    ArrayList<User> userList = new ArrayList<User>();
     
-    System.out.println("Welcome to Tutoring Center!");
+    while(login == false){
+      System.out.println("Do you want to log in or create a account?");
+      System.out.println("C to Create New Account " + "\nL to Login");
 
-    System.out.println("Username: ");
-    String userName = scan.nextLine();
-    
-    System.out.println("Email: ");
-    String email = scan.nextLine();
-    
-    System.out.println("Password: ");
-    String password = scan.nextLine();
-    
-    
+      if(scan.nextLine().equals("C")){
+        System.out.println("Welcome to Tutoring Center!");
 
-    // User user = new User(username, password, email)
+        System.out.println("Enter new username: ");
+        String userName = scan.nextLine();
+      
+        System.out.println("Enter your email: ");
+        String email = scan.nextLine();
+      
+        System.out.println("Create new password: ");
+        String password = scan.nextLine();
+  
+        User user = new User(userName, password, email);
+
+        userList.add(user);
+        
+      } else {
+
+        System.out.println("Username: ");
+        String userName = scan.nextLine();
+
+        System.out.println("Password: ");
+        String password = scan.nextLine();
+
+        for(int i = 0; i < userList.size(); i++){
+          if(userList.get(i).getAccountUsername().equals(userName)){
+            if(userList.get(i).getPassword().equals(password)){
+              System.out.println("Successfully Signed In!");
+              break;
+            }
+          }
+        }
+        
+        System.out.println("Welcome Back");
+        login = true;
+      }
+      
+    }    
   }
 }
