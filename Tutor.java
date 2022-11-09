1a8165a1fb8d31b77422976ea90528cc76b36e18
aschidamb <112208471+aschidamb@users.noreply.github.com>
2022-11-09 14:46:51 -0500

    Update Tutor.java

--- Tutor.java
+++ Tutor.java
@@ -1,9 +1,11 @@
 public class Tutor extends User {
   private String[] subjects;
+  double price;
   
-  public Tutor(String accountUsername, String password, String email, boolean[] blockedList, String[] subjects){
+  public Tutor(String accountUsername, String password, String email, boolean[] blockedList, String[] subjects, double price){
     super(accountUsername, password, email, blockedList);
     this.subjects = subjects;
+    this.price = price;
   }
   
   public String[] getSubjects(){
@@ -12,5 +14,13 @@ public class Tutor extends User {
   
   public void setSubjects(String[] subjects){
     this.subjects = subjects;
+  }
+  
+  public double price(){
+    return price;
+  }
+  
+  public void setPrice(double price){
+    this.price = price;
   }
 }
