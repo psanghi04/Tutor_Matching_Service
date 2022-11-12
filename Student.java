b64a8e30c24d83f8c3c0a1322a105af151461cbe
isha-singhal1 <112501154+isha-singhal1@users.noreply.github.com>
2022-11-12 15:11:21 -0500

    Updated Student class

--- Student.java
+++ Student.java
@@ -1,6 +1,6 @@
 public class Student extends User {
 
-  public Student(String message, String accountUsername, String email, User[] blockedList){
+  public Student(String message, String accountUsername, String email, User[] blockedList) {
     super(message, accountUsername, email, blockedList);
   } 
   
