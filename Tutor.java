8b74344e4b5ace792b431c02e709401d3bd91aaf
isha-singhal1 <112501154+isha-singhal1@users.noreply.github.com>
2022-11-12 15:12:05 -0500

    Updated Tutor class

--- Tutor.java
+++ Tutor.java
@@ -3,38 +3,38 @@ public class Tutor extends User {
   double price;
   ArrayList<Store> stores = new ArrayList<Stores>();
   
-  public Tutor(String accountUsername, String password, String email, User[] blockedList, String[] subjects, double price, ArrayList<Store> stores){
+  public Tutor(String accountUsername, String password, String email, User[] blockedList, String[] subjects, double price, ArrayList<Store> stores) {
     super(accountUsername, password, email, blockedList);
     this.subjects = subjects;
     this.price = price;
     this.stores = stores;
   }
   
-  public String[] getSubjects(){
+  public String[] getSubjects() {
     return subjects;
   }
   
-  public void setSubjects(String[] subjects){
+  public void setSubjects(String[] subjects) {
     this.subjects = subjects;
   }
   
-  public double price(){
+  public double price() {
     return price;
   }
   
-  public void setPrice(double price){
+  public void setPrice(double price) {
     this.price = price;
   }
   
-  public ArrayList<Store> getStores(){
+  public ArrayList<Store> getStores() {
     return stores;
   }
   
-  public ArrayList<Store> setStores(ArrayList<Store> stores){
+  public ArrayList<Store> setStores(ArrayList<Store> stores) {
     this.stores = new ArrayList<Store>();
   }
   
-  public void addStore(Store store){
+  public void addStore(Store store) {
     stores.add(store);
   }
 }
