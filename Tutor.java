91d57939500ff4f186c7482ac40f429da270005e
Jerry <jerrysong1128@gmail.com>
2022-11-12 21:23:51 -0500

    student.java

--- Tutor.java
+++ Tutor.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
 public class Tutor extends User {
   private String[] subjects;
   double price;
-  ArrayList<Store> stores = new ArrayList<Stores>();
+  ArrayList<Store> stores = new ArrayList<Store>();
   
-  public Tutor(String accountUsername, String password, String email, User[] blockedList, String[] subjects, double price, ArrayList<Store> stores) {
-    super(accountUsername, password, email, blockedList);
+  public Tutor(String accountUsername, String password, String email, ArrayList<User> blockedList,ArrayList<User> invisibleList, String[] subjects, double price, ArrayList<Store> stores) {
+    super(accountUsername, password, email, blockedList, invisibleList);
     this.subjects = subjects;
     this.price = price;
     this.stores = stores;
@@ -30,7 +31,7 @@ public class Tutor extends User {
     return stores;
   }
   
-  public ArrayList<Store> setStores(ArrayList<Store> stores) {
+  public void setStores(ArrayList<Store> stores) {
     this.stores = new ArrayList<Store>();
   }
   
