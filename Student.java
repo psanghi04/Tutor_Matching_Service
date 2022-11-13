7dc794afef7e6e6be23422b4f8000b0ca83c5930
Jerry <jerrysong1128@gmail.com>
2022-11-12 21:22:29 -0500

    Student.java

--- Student.java
+++ Student.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
 public class Student extends User {
 
-  public Student(String message, String accountUsername, String email, User[] blockedList) {
-    super(message, accountUsername, email, blockedList);
+  public Student(String message, String accountUsername, String email, ArrayList<User> blockedList, ArrayList<User> invisibleList) {
+    super(message, accountUsername, email, blockedList, invisibleList);
   } 
   
 }
