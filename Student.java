14b7b3739f557316c5933c277a69d5c9dd02e3f3
aschidamb <112208471+aschidamb@users.noreply.github.com>
2022-11-20 19:16:20 -0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Student extends User {
 
-    public Student(String message, String accountUsername, String email, ArrayList<User> blockedList, ArrayList<User> invisibleList) {
+    public Student(String message, String accountUsername, String email, ArrayList<String> blockedList, ArrayList<String> invisibleList) {
         super(message, accountUsername, email, blockedList, invisibleList);
     }
 
