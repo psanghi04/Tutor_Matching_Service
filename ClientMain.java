c18d357ff8de430c2069d7f13f3f61ba875d3b65
songJ1128 <112183586+songJ1128@users.noreply.github.com>
2022-11-29 19:21:17 -0500

    Delete ClientMain.java

--- ClientMain.java
+++
@@ -1,8 +0,0 @@
-public class ClientMain {
-    public static void main(String[] args) {
-            MyRunnable newR = new MyRunnable();
-            Thread thread1 = new Thread(newR);
-            thread1.start();
-        }
-
-    }
