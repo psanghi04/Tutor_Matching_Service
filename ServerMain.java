4517e44928dbb389f5e133e6a8e9475840ec1930
songJ1128 <112183586+songJ1128@users.noreply.github.com>
2022-11-29 19:23:40 -0500

    Update ServerMain.java

--- ServerMain.java
+++ ServerMain.java
@@ -14,8 +14,8 @@ public class ServerMain {
         }
         BufferedReader bfrFile = new BufferedReader(new FileReader(f));
         PrintWriter pwFile = new PrintWriter(new FileWriter(f));
-        int loginOrNewAcc = Integer.parseInt(bfr.readLine());
-        System.out.println(loginOrNewAcc);
+        int loginOrNewAcc = Integer.parseInt(bfr.readLine()); //bfr not reading 
+        System.out.println(loginOrNewAcc); 
         if(loginOrNewAcc == 1){
             String username = bfr.readLine();
             String password = bfr.readLine();
