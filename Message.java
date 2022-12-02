7fe23b0e4fc878a788542b1f0009b3269b6cb069
Pranav Sanghi <112352133+psanghi04@users.noreply.github.com>
2022-12-02 14:13:15 -0500

    Copy from main branch

--- Message.java
+++ Message.java
@@ -2,34 +2,18 @@ import java.io.*;
 import java.sql.*;
 import java.time.*;
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class Message {
-    public boolean isBlocked(User user, String personBlocked){
-//        ArrayList<String> blocked = new ArrayList<String>();
-//        for(int i=0;i<user.getBlockedList().size();i++){
-//            blocked.add(user.getBlockedList().get(i));
-//        }
-//        for (String s : blocked) {
-//            if (s.equals(personBlocked)) {
-//                try {
-//                    File f = new File(user.getAccountUsername() + "_" + personBlocked);
-//                    BufferedWriter bfw = new BufferedWriter(new FileWriter(f));
-//                    bfw.write("This person is blocked");
-//                    bfw.close();
-//                    return true;
-//                    // break;
-//                } catch (IOException e) {
-//                    System.out.println("This chat does not exist");
-//                }
-//            }
-//        }
-//        return false;
+    public boolean isBlocked(User user, User personBlocked){
+        
         String[] lineArr;
-        try (BufferedReader bfr = new BufferedReader(new FileReader("BlockedUsers.txt"))) {
+        try (BufferedReader bfr = new BufferedReader(new FileReader("InvisibleUsers.txt"))) {
             String line = bfr.readLine();
             while (line != null) {
                 lineArr = line.split(";");
-                if (lineArr[0].contains(personBlocked) && lineArr[1].equals(user.getAccountUsername())) {
+                if (UUID.fromString(lineArr[0].split(",")[1]).compareTo(personBlocked.getID()) == 0 &&
+                        UUID.fromString(lineArr[1]).compareTo(user.getID()) == 0) {
                     return true;
                 }
                 line = bfr.readLine();
@@ -37,23 +21,18 @@ public class Message {
         } catch (Exception e) {
             return false;
         }
-    return false;
+        return false;
     } // if he is blocked/invisible and correct user
 
-    public boolean isInvisible(User user, String personInvisible){
-//        for(int i=0;i<user.getInvisibleList().size();i++){
-//            if(user.getInvisibleList().get(i).equals(personInvisible)){
-//                return true;
-//            }
-//        }
-//        return false;
+    public boolean isInvisible(User user, User personInvisible){
 
         String[] lineArr;
         try (BufferedReader bfr = new BufferedReader(new FileReader("InvisibleUsers.txt"))) {
             String line = bfr.readLine();
             while (line != null) {
                 lineArr = line.split(";");
-                if (lineArr[0].contains(personInvisible) && lineArr[1].equals(user.getAccountUsername())) {
+                if (UUID.fromString(lineArr[0].split(",")[1]).compareTo(personInvisible.getID()) == 0 &&
+                        UUID.fromString(lineArr[1]).compareTo(user.getID()) == 0) {
                     return true;
                 }
                 line = bfr.readLine();
@@ -63,10 +42,10 @@ public class Message {
         }
         return false;
     }
-    public void edit(User user, String otherPerson, String message, String edit) {
-        ArrayList<String> conversation = new ArrayList<String>();
+    public void edit(User user, User otherPerson, String message, String edit) {
+        ArrayList<String> conversation = new ArrayList<>();
         try {
-            File f = new File(user.getAccountUsername() + "_" + otherPerson);
+            File f = new File(user.getID() + "_" + otherPerson.getID());
             if(!f.exists()){
                 f.createNewFile();
             }
@@ -96,10 +75,10 @@ public class Message {
         }
     }
 
-    public void delete(User user, String otherPerson, String messageDeleted) {
-        ArrayList<String> conversation = new ArrayList<String>();
+    public void delete(User user, User otherPerson, String messageDeleted) {
+        ArrayList<String> conversation = new ArrayList<>();
         try {
-            File f = new File(user.getAccountUsername() + "_" + otherPerson);
+            File f = new File(user.getID() + "_" + otherPerson.getID());
 
             if(f.exists()){
                 f.createNewFile();
@@ -132,10 +111,10 @@ public class Message {
         return currentTime.toString().substring(0, 19); // returns time with precision as seconds
     }
 
-    public ArrayList<String> readMsg(String sender, String receiver) {
+    public ArrayList<String> readMsg(User sender, User receiver) {
         ArrayList<String> messages = new ArrayList<>();
 
-        try (BufferedReader bfr = new BufferedReader(new FileReader(sender + "_" + receiver))) {
+        try (BufferedReader bfr = new BufferedReader(new FileReader(sender.getID() + "_" + receiver.getID()))) {
             String line = bfr.readLine();
             while (line != null) {
                 String message = line.split(",")[1] + ": " + line.split(",")[3];
@@ -153,10 +132,10 @@ public class Message {
         return messages;
     }
 
-    public ArrayList<String> readMsg(String sender, String receiver, boolean ex) {
+    public ArrayList<String> readMsg(User sender, User receiver, boolean ex) {
         ArrayList<String> messages = new ArrayList<>();
 
-        try (BufferedReader bfr = new BufferedReader(new FileReader(sender + "_" + receiver))) {
+        try (BufferedReader bfr = new BufferedReader(new FileReader(sender.getID() + "_" + receiver.getID()))) {
             String line = bfr.readLine();
             while (line != null) {
                 messages.add(line);
@@ -173,10 +152,10 @@ public class Message {
         return messages;
     }
 
-    public void writeMsg(String sender, String receiver, String content) {
+    public void writeMsg(User sender, User receiver, String content) {
 
         try {
-            File f = new File(sender + "_" + receiver);
+            File f = new File(sender.getID() + "_" + receiver.getID());
             if(f.exists()){
                 f.createNewFile();
             }
@@ -186,9 +165,9 @@ public class Message {
             String line = "%s;%s,%s,%s,%s";
             bfw.write(String.format(
                     line,
-                    sender,
-                    receiver,
-                    sender,
+                    sender.getAccountUsername(),
+                    receiver.getAccountUsername(),
+                    sender.getAccountUsername(),
                     this.getTime(),
                     content));
             bfw.newLine();
@@ -198,9 +177,9 @@ public class Message {
         }
     }
 
-    public void export(String sender, String receiver) {
+    public void export(User sender, User receiver) {
         ArrayList<String> messages = readMsg(sender, receiver, true);
-        String fileName = receiver + "_" + sender;
+        String fileName = receiver.getID() + "_" + sender.getID();
         File f = new File(fileName);
 
         try{
@@ -218,8 +197,6 @@ public class Message {
         } catch (IOException e){
             System.out.println("Error!");
         }
-
-
     }
 
 }
