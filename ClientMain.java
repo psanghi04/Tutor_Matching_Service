5b03fc12ea6fd3456383ad9369f1e348887ade69
Jerry <jerrysong1128@gmail.com>
2022-12-02 14:35:04 -0500

    Hey :wq
    h Pleast enter the commit message for your changes. Lines starting

--- ClientMain.java
+++ ClientMain.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -15,6 +16,13 @@ public class ClientMain {
     private static JButton createAcc;
 
     private static JButton signIn;
+    private static JFrame frame1;
+    private static JButton loginButton;
+    private static JLabel usernamePrompt;
+    private static JTextField usernameField;
+    private static JLabel passwordPrompt;
+    private static JTextField passwordField;
+    private static int successfulLogin;
 
     public static void createJFrame() {
         frame = new JFrame("Welcome");
@@ -65,7 +73,6 @@ public class ClientMain {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     System.out.println("Create Account Selected successfully");
-                    SwingUtilities.invokeLater(new CreateNewAccount());
                 }
             });
 
@@ -73,7 +80,60 @@ public class ClientMain {
                 @Override
                 public void actionPerformed(ActionEvent e) {
                     System.out.println("Log In Selected successfully");
-                    SwingUtilities.invokeLater(new MyRunnable());
+                    try {
+                        Socket socket = new Socket("localhost", 9648);
+                        BufferedReader bfr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                        PrintWriter pw = new PrintWriter(socket.getOutputStream());
+                        createJFrame();
+                        frame1.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                        JPanel panelOne = new JPanel(new GridLayout(0,1,10,5));
+                        JPanel panelTwo = new JPanel();
+                        content.add(panelOne, BorderLayout.NORTH);
+                        usernamePrompt = new JLabel("Username:");
+                        loginButton = new JButton("Login");
+                        usernameField = new JTextField(10);
+                        usernameField.setText("Username");
+                        passwordPrompt = new JLabel("Password:");
+                        passwordField = new JTextField(10);
+                        passwordField.setText("Password");
+                        loginButton.setPreferredSize(new Dimension(40, 40));
+                        panelOne.add(usernamePrompt);
+                        panelOne.add(usernameField);
+                        panelOne.add(passwordPrompt);
+                        panelOne.add(passwordField);
+                        panelOne.add(loginButton);
+                        frame1.setVisible(true);
+
+                        loginButton.addActionListener(new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+
+                                pw.println(1);
+
+                                pw.flush();
+
+                                pw.println(usernameField.getText());
+
+                                pw.flush();
+
+                                pw.println(passwordField.getText());
+
+                                pw.flush();
+
+                                try {
+                                    successfulLogin = Integer.parseInt(bfr.readLine()); //bfr is not reading from the server
+                                    System.out.println(successfulLogin);
+                                    //frame1.dispose();
+                                } catch (IOException ex) {
+                                    JOptionPane.showMessageDialog(null,"Unsuccessful Login", "Error", JOptionPane.ERROR_MESSAGE);
+                                    ex.printStackTrace();
+                                }
+                            }
+                        });
+                    } catch(Exception ex) {
+                        JOptionPane.showMessageDialog(null,"Error","Error",JOptionPane.ERROR_MESSAGE);
+                        ex.printStackTrace();
+                    }
                 }
             });
         } catch(Exception e) {
