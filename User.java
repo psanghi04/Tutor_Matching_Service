0b7e557b6e9fefcc56d4b93240efe721a46d2113
Pranav Sanghi <112352133+psanghi04@users.noreply.github.com>
2022-12-02 14:15:40 -0500

    Copy from main branch

--- User.java
+++ User.java
@@ -1,18 +1,29 @@
 import java.util.ArrayList;
+import java.util.UUID;
 
 public class User {
     private String accountUsername; // username of user account
     private String password; // password of user account
     private String email; // email of user account
-    private ArrayList<String> blockedList; // list of all blocked users
-    private ArrayList<String> invisibleList;
 
-    public User(String accountUsername, String password, String email, ArrayList<String> blockedList, ArrayList<String> invisibleList) {
+    private final UUID ID;
+
+    public User(String accountUsername, String password, String email) {
         this.accountUsername = accountUsername; // sets the current instance of accountUsername to the parameter accountUsername
         this.password = password; // sets the current instance of password to the parameter password
         this.email = email; // sets the current instance of email to the parameter email
-        this.blockedList = blockedList; // sets the current instance of blockedList to the parameter blockedList
-        this.invisibleList = invisibleList;
+        this.ID = UUID.randomUUID();
+    }
+
+    public User(String accountUsername, String password, String email, UUID ID) {
+        this.accountUsername = accountUsername; // sets the current instance of accountUsername to the parameter accountUsername
+        this.password = password; // sets the current instance of password to the parameter password
+        this.email = email; // sets the current instance of email to the parameter email
+        this.ID = ID;
+    }
+
+    public UUID getID() {
+        return ID;
     }
 
     // get account username
@@ -30,11 +41,6 @@ public class User {
         return email;
     }
 
-    // get isBlocked
-    public ArrayList<String> getBlockedList() {
-        return blockedList;
-    }
-
     // set accountUsername
     public void setAccountUsername(String accountUsername) {
         this.accountUsername = accountUsername;
@@ -48,19 +54,6 @@ public class User {
     // set email
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    // set blocked status
-    public void setBlockedList(ArrayList<String> blockedList) {
-        this.blockedList = blockedList;
-    }
-
-    public ArrayList<String> getInvisibleList() {
-        return invisibleList;
-    }
-
-    public void setInvisibleList(ArrayList<String> invisibleList) {
-        this.invisibleList = invisibleList;
     }
 
 }
